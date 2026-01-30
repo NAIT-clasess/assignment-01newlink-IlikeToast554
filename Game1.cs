e3624ae478671b0cb56a86d18ede2773b8d0110b
Aiden Hartman-Harvey <aidenharvey414@gmail.com>
2026-01-30 09:25:30 -0700

    Made it so One of the astronauts moves diagonaly by its self

--- Game1.cs
+++ Game1.cs
@@ -12,10 +12,15 @@ public class Game1 : Game
 
     private Texture2D _SpaceBackground;
     private Texture2D _Astronaut;
+    private Texture2D _Astronaut2;
 
     private SpriteFont _Arial;
 
     private SimpleAnimation _jupiterAnimation;
+
+    private Vector2 _velocity;
+    private Vector2 _position;
+    private float _speed = 50f;
 
     public Game1()
     {
@@ -24,14 +29,23 @@ public class Game1 : Game
         IsMouseVisible = true;
     }
 
+    protected override void Initialize()
+    {
+        _position = new Vector2(100, 100);
+        _velocity = new Vector2(-1f, -1f); 
+
+        base.Initialize();
+    }
+
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
         _SpaceBackground = Content.Load<Texture2D>("Space Background");
         _Astronaut = Content.Load<Texture2D>("ASTRONAUT");
-        _Arial = Content.Load<SpriteFont>("File");
+        _Astronaut2 = Content.Load<Texture2D>("ASTRONAUT2");
 
+        _Arial = Content.Load<SpriteFont>("File");
 
         _jupiterAnimation = new SimpleAnimation(Content.Load<Texture2D>("jupiter-128x128"), 128, 128, 6, 1);
 
@@ -40,6 +54,11 @@ public class Game1 : Game
     protected override void Update(GameTime gameTime)
     {
         _jupiterAnimation.Update(gameTime);
+
+        float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+        _position += _velocity * _speed * deltaTime;
+
+        
 
         base.Update(gameTime);
     }
@@ -52,7 +71,8 @@ public class Game1 : Game
 
         _spriteBatch.Draw(_SpaceBackground, new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height), Color.White);
 
-        _spriteBatch.Draw(_Astronaut, new Vector2(100, 100), Color.White);
+        _spriteBatch.Draw(_Astronaut, _position, Color.White);
+        _spriteBatch.Draw(_Astronaut2, new Vector2(100, 200), Color.White);
 
         _jupiterAnimation.Draw(_spriteBatch, new Vector2(500, 100), SpriteEffects.None);
 
@@ -62,4 +82,5 @@ public class Game1 : Game
 
         base.Draw(gameTime);
     }
+
 }
